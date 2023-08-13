ccb35a96cfc7ead006cec858ea10ab233cbcc9ee
xiaozhenxu <xiaozhenxu@zju.edu.cn>
2023-08-13 07:53:16 -0700

    在 syscall 函数中增加显示代码

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -131,6 +131,30 @@ static uint64 (*syscalls[])(void) = {
 [SYS_trace]   sys_trace,
 };
 
+static char* syscall_names[] = {
+  [SYS_fork]    "sys_fork",
+  [SYS_exit]    "sys_exit",
+  [SYS_wait]    "sys_wait",
+  [SYS_pipe]    "sys_pipe",
+  [SYS_read]    "sys_read",
+  [SYS_kill]    "sys_kill",
+  [SYS_exec]    "sys_exec",
+  [SYS_fstat]   "sys_fstat",
+  [SYS_chdir]   "sys_chdir",
+  [SYS_dup]     "sys_dup",
+  [SYS_getpid]  "sys_getpid",
+  [SYS_sbrk]    "sys_sbrk",
+  [SYS_sleep]   "sys_sleep",
+  [SYS_open]    "sys_open",
+  [SYS_write]   "sys_write",
+  [SYS_mknod]   "sys_mknod",
+  [SYS_unlink]  "sys_unlink",
+  [SYS_link]    "sys_link",
+  [SYS_mkdir]   "sys_mkdir",
+  [SYS_close]   "sys_close",
+  [SYS_trace]   "sys_trace",
+};
+
 void
 syscall(void)
 {
@@ -140,6 +164,12 @@ syscall(void)
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
+    
+    /* 判断当前系统调用是否需要被追踪 */
+    if((p->tracemask & (1<<num))!=0)
+    {
+      printf("%d: syscall %s -> %d\n", p->pid, syscall_names[num], p->trapframe->a0);
+    }
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);

9a67f55fa69ea66a8423c1f969a0200f08a85124
xiaozhenxu <xiaozhenxu@zju.edu.cn>
2023-08-19 22:59:55 -0700

    cow:修改fork

--- kernel/vm.c
+++ kernel/vm.c
@@ -311,12 +311,20 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
-    flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
       goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+
+    // memmove(mem, (char*)pa, PGSIZE);
+    // if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
+    //   kfree(mem);
+    //   goto err;
+    // }
+
+    *pte &= ~PTE_W;
+    *pte |= PTE_COW;
+    // 获得标志位
+    flags = PTE_FLAGS(*pte);
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
       goto err;
     }
   }
@@ -431,4 +439,37 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+
+int isCowPage(pagetable_t pagetable, uint64 va)
+{
+  pte_t* pte = walk(pagetable,va, 0);
+  if(pte==0) return 0;
+  if((*pte & PTE_V)==0) return 0;
+  if((*pte & PTE_U)==0) return 0;
+
+  if((*pte & PTE_COW)) return 1;
+  return 0;
+}
+
+int cowAlloc(pagetable_t pagetable, uint64 va)
+{
+  va = PGROUNDDOWN(va);
+  pte_t* pte = walk(pagetable,va,0);
+  uint64 pa = PTE2PA(*pte);
+  int flag = PTE_FLAGS(*pte);
+  char* mem = kalloc();
+  if(mem==0) return -1;
+  memmove(mem,(char*)pa,PGSIZE);
+  // 解除映射并释放物理内存
+  uvmunmap(pagetable, va, 1, 1);
+  
+  flag &= ~PTE_COW;
+  flag |= PTE_W;
+  if(mappages(pagetable, va, PGSIZE, (uint64)mem, flag)<0)
+  {
+    kfree(mem);
+    return -1;
+  }
+  return 0;
 }

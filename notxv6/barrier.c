1e43b91d79c2934f5ac6b551d06280ed93b7fb98
xiaozhenxu <xiaozhenxu@zju.edu.cn>
2023-08-23 03:04:36 -0700

    完成 barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -31,6 +31,18 @@ barrier()
   // then increment bstate.round.
   //
   
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread += 1;
+  if(bstate.nthread != nthread)
+  {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  else{
+    bstate.round += 1;
+    bstate.nthread = 0;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
